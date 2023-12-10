3f773d07199052381114295cc443ba4f133699ae
Ngakan Gandhi <n.nyoman.gandhi@gmail.com>
2023-12-11 05:32:19 +0700

    checkpoint

--- python/objects-and-classes/classes.py
+++ python/objects-and-classes/classes.py
@@ -13,12 +13,15 @@ def square_new(name, side):
         "_class": Square
     }
 
+def square_larger(thing, size):
+    return call(thing, "area") > size
+
 Square = {
     "perimeter": square_perimeter,
     "area": square_area,
+    "larger": square_larger,
     "_classname": "Square"
 }
-
 def circle_perimeter(thing: dict) -> float:
     return math.pi * thing["radius"] * 2
 
@@ -32,19 +35,25 @@ def circle_new(name, radius):
         "_class": Circle
     }
 
+def circle_larger(thing, size):
+    return call(thing, "area") > size
+
 Circle = {
     "perimeter": circle_perimeter,
     "area": circle_area,
+    "larger": circle_larger,
     "_classname": "Circle"
 }
 
-def call(thing, method_name):
-    return thing["_class"][method_name](thing)
+def call(thing, method_name, *args):
+    return thing["_class"][method_name](thing, *args)
 
 examples = [square_new("sq", 3), circle_new("ci", 2)]
 for ex in examples:
     n = ex["name"]
     p = call(ex, "perimeter")
     a = call(ex, "area")
+    larger = call(ex, "larger", 10)
     c = ex["_class"]["_classname"]
     print(f"{n} is a {c}: {p:.2f} {a:.2f}")
+    print(f"is {ex['name']} larger? {larger}")
--- python/objects-and-classes/varargs.py
+++ python/objects-and-classes/varargs.py
@@ -5,3 +5,12 @@ show_args("nothing")
 show_args("one unnamed argument", 1)
 show_args("one named argument", second="2")
 show_args("one of each", 3, fourth="4")
+
+def show_spread(left, middle, right):
+    print(f"left {left} middle {middle} right {right}")
+
+all_in_list = [1, 2, 3]
+show_spread(*all_in_list)
+
+all_in_dict = {"right": 30, "left": 10, "middle": 20}
+show_spread(**all_in_dict)
